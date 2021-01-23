7c52c5d9c6996a52bf5b8ced902f54fb0a48609c
BURCU ÖZŞAHİN <15050111026@ybu.edu.tr>
2021-01-23 16:25:02 +0300

    Add project files.

---
+++ MyDictionaryImplementation/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryImplementation
+{
+    class MyDictionary<K,V>
+    {
+        K[] key;
+        V[] value;
+        K[] tempKey;
+        V[] tempValue;
+        public MyDictionary()
+        {
+            key = new K[0];
+            value = new V[0];
+        }
+        public void Add(K candidateKey, V candidateValue)
+        {
+            
+            tempKey = key;
+            tempValue = value;
+
+            key = new K[key.Length + 1];
+            value = new V[value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+
+            }
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+
+                value[i] = tempValue[i];
+            }
+
+            key[key.Length - 1] = candidateKey;
+            value[value.Length - 1] = candidateValue;
+
+
+
+        }
+       
+
+        public int Count { get { return key.Length; } }
+        public K[] Keys { get { return key; } }
+        public V[] Values { get { return value; } }
+    }
+    }
+
---
+++ MyDictionaryImplementation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary <int,string> myDictionary = new MyDictionary<int,string>();
+            myDictionary.Add(101, "Calculus");
+            myDictionary.Add(201, "Operating System");
+            myDictionary.Add(301, "Software Engineering");
+
+            Console.WriteLine("Count: "+myDictionary.Count);
+           
+            
+        }
+    }
+}
